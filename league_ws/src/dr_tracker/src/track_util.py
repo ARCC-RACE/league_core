1f67c6f3d94e113c673bdb296fecfea9150b3a62
Michael Equi <equim20@student.jhs.net>
2019-10-06 22:13:30 -0700

    Now detecting if car is on or off the track

--- league_ws/src/dr_tracker/src/track_util.py
+++ league_ws/src/dr_tracker/src/track_util.py
@@ -5,18 +5,25 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-import math
 from nav_msgs.msg import Odometry
-from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Bool
 import time
-import tf
+
+
+def constrain(x, min, max):
+    if x < min:
+        return min
+    elif x > max:
+        return max
+    else:
+        return x
 
 
 class TrackUtil():
 
     def __init__(self, debug_mode=True, temporal_filtering=2, line_contour_cutoff=100, cam_dist_from_ground=1,
-                 camera_vertical_fov=80, camera_horizontal_fov=120, track_line_lower=None,
-                 track_line_upper=None, blur_param=9, refresh_rate=0):
+                 camera_vertical_fov=80, camera_horizontal_fov=120, dr_height=0.254, track_line_lower=None,
+                 track_line_upper=None, blur_param=9, refresh_rate=0, center_point=(480, 100)):
         if track_line_lower is None:
             track_line_lower = [0, 0, 0]
         if track_line_upper is None:
@@ -25,13 +32,17 @@ class TrackUtil():
         self.debug = debug_mode
         self.temporal_filtering = temporal_filtering
         self.line_contour_cutoff = line_contour_cutoff
-        self.cam_dist_from_groun = cam_dist_from_ground
+        self.cam_dist_from_ground = cam_dist_from_ground
         self.camera_vertical_fov = camera_vertical_fov
         self.camera_horizontal_fov = camera_horizontal_fov
         self.track_line_lower = track_line_lower
         self.track_line_upper = track_line_upper
         self.blur_param = blur_param
         self.refresh_rate = refresh_rate
+        self.center_point = center_point
+        self.dr_height = dr_height
+
+        self.track_status_pub = rospy.Publisher("is_off_track", Bool, queue_size=1)
 
         self.last_image_time = 0
         self.cv_image_filtered = None
@@ -39,8 +50,6 @@ class TrackUtil():
         self.camera_resolution = None
         self.parsed_img = None
         self.bridge = CvBridge()
-
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.is_off_track, oneshot=False)
 
     def process_image(self, image):  # callback for ROS subscriber with Image message
         """Takes in image from ROS and converts it to open CV form. Begins the process of analyzing the track, the deepracer, etc."""
@@ -61,7 +70,8 @@ class TrackUtil():
         if self.debug and self.cv_image_raw is not None:
             cv2.imshow("Raw image window", self.cv_image_raw)
             cv2.imshow("Filtered image window", self.cv_image_filtered)
-            cv2.imshow("parsed image window", cv2.bitwise_and(self.cv_image_raw, self.cv_image_raw, mask=self.parsed_img))
+            cv2.imshow("parsed image window",
+                       cv2.bitwise_and(self.cv_image_raw, self.cv_image_raw, mask=self.parsed_img))
             # cv2.imshow("Filtered and parsed", np.hstack([self.cv_image_filtered, self.parsed_img]))
             cv2.waitKey(50)
 
@@ -76,7 +86,6 @@ class TrackUtil():
             mask1 = np.zeros(eroded_mask.shape, dtype=np.uint8)  # outer + inner track
             mask2 = np.zeros(eroded_mask.shape, dtype=np.uint8)  # inner track
             _, contours, _ = cv2.findContours(eroded_mask, 1, 2)  # find contour in mask for white lines
-            center_point = (480, 100)  # center for revolving lines around to fill track
             cnts = []
             for i, cnt in enumerate(contours):
                 area = cv2.contourArea(cnt)
@@ -90,22 +99,37 @@ class TrackUtil():
                 for x in range(mask1.shape[0]):
                     for y in range(mask1.shape[1]):
                         if mask1[x, y] > 200:
-                            cv2.line(mask1, (y, x), center_point, 150, 5)
+                            cv2.line(mask1, (y, x), self.center_point, 150, 5)
 
                 cv2.drawContours(mask2, contours, cnts[1], 255, -1)  # Draw filled contour in mask
                 for x in range(mask2.shape[0]):
                     for y in range(mask2.shape[1]):
                         if mask2[x, y] > 200:
-                            cv2.line(mask2, (y, x), center_point, 150, 5)
+                            cv2.line(mask2, (y, x), self.center_point, 150, 5)
                 if self.debug:
-                    cv2.circle(mask2, center_point, 5, 200, thickness=10)
-                    cv2.circle(mask1, center_point, 5, 200, thickness=10)
+                    cv2.circle(mask2, self.center_point, 5, 200, thickness=10)
+                    cv2.circle(mask1, self.center_point, 5, 200, thickness=10)
                 self.parsed_img = cv2.bitwise_xor(mask1, mask2)
             else:
                 self.cv_image_raw = None  # reload an image due to inadequacy of original
 
-    def is_off_track(self, event):
-        pass
+    def is_off_track(self, odom):
+        if self.parsed_img is not None:
+            # convert x,y position back to pixels (inverse operation)
+            dr_position = np.array([odom.pose.pose.position.x, odom.pose.pose.position.y])
+            camera_fov = np.array([self.camera_horizontal_fov, self.camera_vertical_fov])
+            centroid = np.abs((dr_position / (
+                    2.0 * (self.cam_dist_from_ground - self.dr_height) * (
+                np.tan(np.radians(camera_fov / 2.0))))) * self.camera_resolution)
+            if self.debug:
+                print("Position of car in pixels: " + str(centroid))
+            centroid_pixels = [constrain(centroid.astype(int)[0], 0, self.camera_resolution[0]), constrain(centroid.astype(int)[1], 0, self.camera_resolution[1])]
+            if self.parsed_img[centroid_pixels[1], centroid_pixels[0]] > 100:
+                self.track_status_pub.publish(Bool(False))
+            else:
+                self.track_status_pub.publish(Bool(True))
+        else:
+            self.track_status_pub.publish(Bool(True))
 
 
 if __name__ == "__main__":
@@ -119,15 +143,21 @@ if __name__ == "__main__":
     camera_vertical_fov_param = rospy.get_param("~camera_vertical_fov", 80)
     camera_horizontal_fov_param = rospy.get_param("~camera_horizontal_fov", 120)
     blur_param = rospy.get_param("~blur_param", 9)
+    dr_height_param = rospy.get_param("~dr_height", 0.0254)
     input_camera_topic_param = rospy.get_param("~camera_topic", "/usb_cam/image_raw")
+    odom_topic = rospy.get_param("~odom_topic", "/odometry/filtered")
     white_mask = map(int, rospy.get_param("~white_mask", "0, 0, 0, 180, 50, 255").split(','))
+    center_point = map(int, rospy.get_param("~center_point", "0, 0, 0, 180, 50, 255").split(','))
     refresh_param = rospy.get_param('~refresh_rate', 10)
 
     track = TrackUtil(debug_param, temporal_filtering_param, line_contour_cutoff,
-                      cam_dist_from_ground_param, camera_vertical_fov_param, camera_horizontal_fov_param,
+                      cam_dist_from_ground_param, camera_vertical_fov_param, dr_height=dr_height_param,
+                      camera_horizontal_fov=camera_horizontal_fov_param,
                       blur_param=blur_param, refresh_rate=refresh_param,
                       track_line_lower=[white_mask[0], white_mask[1], white_mask[2]],
-                      track_line_upper=[white_mask[3], white_mask[4], white_mask[5]])
+                      track_line_upper=[white_mask[3], white_mask[4], white_mask[5]],
+                      center_point=(center_point[0], center_point[1]))
 
     rospy.Subscriber(input_camera_topic_param, Image, track.process_image)
+    rospy.Subscriber(odom_topic, Odometry, track.is_off_track)
     rospy.spin()
