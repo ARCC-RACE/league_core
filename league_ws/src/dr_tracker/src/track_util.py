0cdf1c5569cd57f47bd7fbd8c57e43a20c243efb
Michael Equi <equim20@student.jhs.net>
2019-10-07 20:42:47 -0700

    minor updates and track detection improvements with track_mask

--- league_ws/src/dr_tracker/src/track_util.py
+++ league_ws/src/dr_tracker/src/track_util.py
@@ -28,7 +28,6 @@ class TrackUtil():
             track_line_lower = [0, 0, 0]
         if track_line_upper is None:
             track_line_upper = [180, 50, 255]
-
         self.debug = debug_mode
         self.temporal_filtering = temporal_filtering
         self.line_contour_cutoff = line_contour_cutoff
@@ -89,19 +88,20 @@ class TrackUtil():
             cnts = []
             for i, cnt in enumerate(contours):
                 area = cv2.contourArea(cnt)
-                if self.line_contour_cutoff < area < 1500:
+                if self.line_contour_cutoff < area:
                     cnts.append(i)
                     if self.debug:
                         print("Area of contour " + str(i) + ": " + str(area))
 
             if len(cnts) > 1:
+                # mask1 = outer, mask2 = inner track
                 cv2.drawContours(mask1, contours, cnts[0], 255, -1)  # Draw filled contour in mask
+                cv2.drawContours(mask2, contours, cnts[1], 255, -1)  # Draw filled contour in mask
+                track_mask = cv2.bitwise_or(cv2.dilate(mask1, np.ones((7, 7), np.uint8), iterations=1), cv2.dilate(mask2, np.ones((7, 7), np.uint8), iterations=1))
                 for x in range(mask1.shape[0]):
                     for y in range(mask1.shape[1]):
                         if mask1[x, y] > 200:
                             cv2.line(mask1, (y, x), self.center_point, 150, 5)
-
-                cv2.drawContours(mask2, contours, cnts[1], 255, -1)  # Draw filled contour in mask
                 for x in range(mask2.shape[0]):
                     for y in range(mask2.shape[1]):
                         if mask2[x, y] > 200:
@@ -109,7 +109,7 @@ class TrackUtil():
                 if self.debug:
                     cv2.circle(mask2, self.center_point, 5, 200, thickness=10)
                     cv2.circle(mask1, self.center_point, 5, 200, thickness=10)
-                self.parsed_img = cv2.bitwise_xor(mask1, mask2)
+                self.parsed_img = cv2.bitwise_or(cv2.bitwise_xor(mask1, mask2), track_mask)
             else:
                 self.cv_image_raw = None  # reload an image due to inadequacy of original
 
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     input_camera_topic_param = rospy.get_param("~camera_topic", "/usb_cam/image_raw")
     odom_topic = rospy.get_param("~odom_topic", "/odometry/filtered")
     white_mask = map(int, rospy.get_param("~white_mask", "0, 0, 0, 180, 50, 255").split(','))
-    center_point = map(int, rospy.get_param("~center_point", "0, 0, 0, 180, 50, 255").split(','))
+    center_point = map(int, rospy.get_param("~center_point", "400, 200").split(','))
     refresh_param = rospy.get_param('~refresh_rate', 10)
 
     track = TrackUtil(debug_param, temporal_filtering_param, line_contour_cutoff,
